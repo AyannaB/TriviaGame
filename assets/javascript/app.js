e423a9d3af913e037e753e651a9ba143b624dfaa
Ayanna Burrowes <a.burrowes529@gmail.com>
2019-08-22 20:33:09 -0400

    updating 3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,34 +3,57 @@ let counter = 35;
 let currentQuestion = 0;
 let correct = 0;
 let wrong = 0;
-let timer;
+let notAnswered = 0;
+// let timer;
+let intervalId;
 
 //Create on click function to start game
 $("#start").on("click", function() {
+  clearInterval(intervalId);
+currentQuestion = 0;
+correct = 0;
+wrong = 0;
+notAnswered = 0; 
+$('#start').hide();
+$('#game').show();
+$('.results').hide();
 generateQuestion();
 });
 
 //Checks if the userAnswer = the actual answer
 $(document).on('click','.choices',function(){
-  let userAnswer = $(this).text();
-  console.log(userAnswer);
-  console.log('it works girl');
+  console.log("this was clicked");
+  let answered = $(this).attr("data-answered")
+  if(answered != "answered"){
+    let userAnswer = $(this).text();
+    console.log(userAnswer);
+    console.log('it works girl');
+  
+    if(userAnswer == quizQuestions[currentQuestion-1].correctAnswer){
+      $(this).css("color", "green");
+      console.log('you got it!');
+      correct++;
+      
+    } else{
+      $(this).css("color", "red");
+      console.log('sis what?');
+      wrong++;
+    }
+    $(".choices").attr("data-answered", "answered");
+    $(".choices").attr("disabled", true);
+  }else{
 
-  if(userAnswer == quizQuestions[currentQuestion-1].correctAnswer){
-    $(this).css("color", "green");
-    console.log('you got it!');
-    correct++;
-    
-  } else{
-    $(this).css("color", "red");
-    console.log('sis what?');
-    wrong++;
   }
+
+  clearInterval(intervalId);
   setTimeout (generateQuestion,1000)
 });
 
 //Generates the questions for the quiz
 function generateQuestion(){
+  let time = 20;
+  $(".time").text('Time Left: ' + time);
+  timer(time);
   if(quizQuestions.length > currentQuestion){
     $(".question").text(quizQuestions[currentQuestion].questions);
     $(".options").empty();
@@ -49,13 +72,40 @@ function generateQuestion(){
 
 }
 
+// Timer stuff
+function timer(time){
+  
+  
+   intervalId = setInterval(count, 1000);
+
+  function count() {
+
+    // DONE: increment time by 1, remember we cant use "this" here.
+    time--;
+  
+    // DONE: Get the current time, pass that into the timeConverter function,
+    //       and save the result in a variable.
+    if(time===0){
+      clearInterval(intervalId);
+      notAnswered++;
+      setTimeout (generateQuestion,2000)
+    }
+    // DONE: Use the variable we just created to show the converted time in the "display" div.
+    $(".time").text('Time Left: ' + time);
+  }
+}
+
 //Showing the results 
   function generateResults(){
+    clearInterval(intervalId);
     $('#game').hide();
     $('.results').show();
-    $('.correct').append(correct);
-    $('.incorrect').append(wrong);
+    $('.correct').text("Correct: " + correct);
+    $('.incorrect').text("Incorrect: " + wrong);
+    $('.notAnswered').text("Not Answered: " + notAnswered)
+    $('#start').show().text('Play Again');
   }
+
 //Create Questions
 const quizQuestions = [
   {
